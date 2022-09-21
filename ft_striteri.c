6843721773b3f731e22c97ea0454b5d6e902db60
Christian Yacoub Obage <cyacoub-@c3r1s4.42madrid.com>
2022-09-21 18:13:04 +0200

    .

--- ft_striteri.c
+++ ft_striteri.c
@@ -6,28 +6,22 @@
 /*   By: cyacoub- <cyacoub-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/21 16:57:13 by cyacoub-          #+#    #+#             */
-/*   Updated: 2022/09/21 16:58:50 by cyacoub-         ###   ########.fr       */
+/*   Updated: 2022/09/21 18:05:20 by cyacoub-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_striteri(char *s, void (*f)(unsigned int, char))
+void	ft_striteri(char *s, void (*f)(unsigned int, char*))
 {
 	unsigned int	i;
-	char			*tmp;
 
 	i = 0;
-	tmp = malloc(ft_strlen(s) + 1);
-	if (tmp == 0)
-		return (0);
-	if (s == 0 || f == 0)
-		return (0);
-	while (s[i])
+	if (!s)
+		return ;
+	while (s[i] != '\0')
 	{
-		tmp[i] = f(i, s[i]);
+		(*f)(i, &s[i]);
 		i++;
 	}
-	tmp[i] = '\0';
-	return (tmp);
 }
