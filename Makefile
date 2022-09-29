6fcb67eb0c346dc16ca874538ec38a6ceed4c0f0
Christian Yacoub Obage <cyacoub-@c1r7s6.42madrid.com>
2022-09-29 14:51:23 +0200

    .

--- Makefile
+++ Makefile
@@ -44,6 +44,12 @@ OBJS = $(SRC:.c=.o)
 BONUS = ft_lstnew.c\
 		ft_lstadd_front.c\
 		ft_lstsize.c\
+		ft_lstadd_back.c\
+		ft_lstlast.c\
+		ft_lstdelone.c\
+		ft_lstclear.c\
+		ft_lstiter.c\
+		ft_lstmap.c
 
 OBJS_BONUS = $(BONUS:.c=.o)
 
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: cyacoub- <cyacoub-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/12 19:01:23 by cyacoub-          #+#    #+#             */
-/*   Updated: 2022/09/28 18:40:57 by cyacoub-         ###   ########.fr       */
+/*   Updated: 2022/09/29 14:49:18 by cyacoub-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,5 +61,11 @@ char	**ft_split(char const *s, char c);
 t_list	*ft_lstnew(void *content);
 void	ft_lstadd_front(t_list **lst, t_list *new);
 int		ft_lstsize(t_list *lst);
+void	ft_lstadd_back(t_list **lst, t_list *new);
+t_list	*ft_lstlast(t_list *lst);
+void	ft_lstdelone(t_list *lst, void (*del)(void *));
+void	ft_lstclear(t_list **lst, void (*del)(void *));
+void	ft_lstiter(t_list *lst, void (*f)(void *));
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 
 #endif
